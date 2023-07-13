bc9e3843f73e862639918a47a0520b32b7eaee2a
Jayant Raj <jayantraj66041@gmail.com>
2023-07-13 23:04:30 +0530

    consumer and routing data connection

--- api/consumers.py
+++ api/consumers.py
@@ -2,10 +2,13 @@ from channels.consumer import SyncConsumer, AsyncConsumer
 from channels.exceptions import StopConsumer
 from random import randint
 from time import sleep
+import json
+from api.models import TradeHistory
 
 
 class StockPriceConsumer(SyncConsumer):
     def websocket_connect(self, event):
+        # Connection stablished
         print("Websocket Connected...", event)
         self.send({
             "type": "websocket.accept"
@@ -15,13 +18,35 @@ class StockPriceConsumer(SyncConsumer):
         print("Websocket Receive...", event)
         print(event['text'])
         while True:
-            stock_price = randint(9000, 11000)
+            actual_price = 10000        # base price of a stock
+            stock_price = randint(9000, 11000)      # random price generated between 9000 and 11000
+
+            # save the stock price in every updation
+            th = TradeHistory()
+            th.bank_name = "ABC Bank"
+            th.trade_value = stock_price
+            th.save()
+
+            # call all 100 latest tradehistory and find the average price
+            all_th = TradeHistory.objects.all().order_by("-id")[:100]
+            avg = 0
+            for th in all_th:
+                avg += th.trade_value
+            avg = avg/100
+
+            # Send the response to the client
             self.send({
                 "type": "websocket.send", 
-                'text': "Stock Price: " + str(stock_price)
+                'text': json.dumps({
+                    "Stock Price": + stock_price,
+                    "Average Price": avg,
+                    "Suggestion": "Sale" if avg>actual_price else "Buy"
+                })
             })
-            sleep(1)
+
+            sleep(2)
 
     def websocket_disconnect(self, event):
+        # connection disconnected.
         print("Websocket Disconnect...", event)
         raise StopConsumer()
--- api/routing.py
+++ api/routing.py
@@ -2,5 +2,6 @@ from django.urls import path
 from api.consumers import StockPriceConsumer
 
 ws_urlpatterns = [
+    # API path
     path("ws/sp/", StockPriceConsumer.as_asgi()),
 ]
